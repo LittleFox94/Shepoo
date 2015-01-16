493f52b735092834d88e69c9e190afeea3f46631
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2015-01-17 00:16:46 +0100

    Added script to configure Shepoo

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 #ifndef _CONFIG_H_INCLUDED
 #define _CONFIG_H_INCLUDED
 
-#define VERSION		"0.1 alpha"
+#define VERSION		"0.1-1b9bc20git"
 #define VERSION_MAJOR	0
 #define VERSION_MINOR	1
 
-#define BLOCK_SIZE	4096
+#define BLOCKSIZE	4096
 
 #endif
---
+++ configure.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+VERSION_MAJOR=0
+VERSION_MINOR=1
+
+DIALOG="whiptail"
+
+function dlg
+{
+	$DIALOG "$@" 3>&1 1>&2 2>&3
+}
+
+function inputSize
+{
+	size=""
+
+	while [[ $size = "" ]]; do
+		read size < <(dlg --inputbox $1 0 40 $2)
+	done
+
+	echo $size;
+}
+
+BLOCKSIZE=$(inputSize "Blocksize" 4096)
+
+dlg --yesno "Do you want to see advanded options?" 0 40
+ADVANCED=$?
+
+VERSION="$VERSION_MAJOR.$VERSION_MINOR"
+
+if [ -e "/usr/bin/git" ]; then
+	VERSION="$VERSION-$(git log -1 --abbrev-commit | head -n 1 | cut -d " " -f 2)git"
+fi
+
+if [ $ADVANCED -eq 0 ]; then
+	VERSION=$(dlg --inputbox "Version-string" 0 40 "$VERSION")
+fi
+
+configContent="#ifndef _CONFIG_H_INCLUDED
+#define _CONFIG_H_INCLUDED
+
+#define VERSION		\"$VERSION\"
+#define VERSION_MAJOR	0
+#define VERSION_MINOR	1
+
+#define BLOCKSIZE	$BLOCKSIZE
+
+#endif"
+
+echo "$configContent" > config.h
